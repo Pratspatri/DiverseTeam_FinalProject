e0620433fc027890cfb6e19f1f34b9484f472e30
malok2012 <malok1433@hotmail.com>
2016-06-14 03:25:49 -0400

    Add PowerArmor
    It is one of armor type

---
+++ src/item/PowerArmor.java
@@ -0,0 +1,25 @@
+package item;
+
+/**
+ * It is one of special armor type 
+ * @author Malak
+ *
+ */
+public class PowerArmor extends SpecialArmor 
+{   
+	/**
+	 * Constructor
+	 */
+	public PowerArmor()
+	{
+		description="powerArmor";
+	}
+
+	@Override
+	public void useArmor(int distance) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
