182312a01aab1c913dcc1e4c9b678d83290e1b3a
malok2012 <malok1433@hotmail.com>
2016-06-14 03:25:14 -0400

    Add NormalArmor
    it is armor without any ability. it is just normal armor

---
+++ src/item/NormalArmor.java
@@ -0,0 +1,24 @@
+package item;
+
+/**
+ * It is one of armor type which does not have any ability
+ * @author Malak
+ *
+ */
+class NormalArmor extends Armor 
+{
+	/**
+	 * Constructor
+	 */
+     public NormalArmor()
+     {
+    	 description="normalArmor";
+     }
+	@Override
+	public void useArmor(int distance) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
