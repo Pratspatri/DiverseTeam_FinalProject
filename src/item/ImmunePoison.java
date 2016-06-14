dd8be014ac2dd898c8e911f01751e6c42e4a4316
malok2012 <malok1433@hotmail.com>
2016-06-14 03:24:24 -0400

    Add ImmunePoison
    It is one of decorator for special armor

---
+++ src/item/ImmunePoison.java
@@ -0,0 +1,24 @@
+package item;
+
+/**
+ * This class will decorator the armor, so player can immune from poison
+ * @author Malak
+ *
+ */
+public class ImmunePoison extends AbilityArmor 
+{
+	/**
+	 * Constructor
+	 */
+	public ImmunePoison()
+	{
+		description="immunepoison";
+	}
+	@Override
+	public void useArmor(int distance) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
