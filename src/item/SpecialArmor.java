25d0fd2239a526b461f1af2bff401b9065ea16b9
malok2012 <malok1433@hotmail.com>
2016-06-14 03:28:01 -0400

    Add SpecialArmor
    It is  an abstarct class contain PwoerArmor and SharpArmor

---
+++ src/item/SpecialArmor.java
@@ -0,0 +1,33 @@
+package item;
+
+/**
+ * It is one of armor type which does have  ability
+ * @author Malak
+ *
+ */
+public abstract class SpecialArmor extends Armor
+{
+	/**
+	 * Describe the armor type
+	 */
+public String description;
+
+/**
+ * set description for a armor
+ * @param description
+ */
+public void setDescription(String description)
+{
+	this.description=description;
+}
+
+/**
+ * get description for a armor
+ * @return description
+ */
+public String getDescription()
+{
+	return description;
+}
+
+}
