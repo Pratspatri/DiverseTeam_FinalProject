8f52ef2bd3ab29cfe992ab58c354ddc3db555278
malok2012 <malok1433@hotmail.com>
2016-06-14 03:26:27 -0400

    Add SharpArmor
    It is one of Armor type

---
+++ src/item/SharpArmor.java
@@ -0,0 +1,24 @@
+package item;
+
+/**
+ * It is one of special armor type 
+ * @author Malak
+ *
+ */
+public class SharpArmor extends SpecialArmor 
+{
+	/**
+	 * Constructor
+	 */
+	public SharpArmor()
+	{
+		description="sharpArmor";
+	}
+	@Override
+	public void useArmor(int distance)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
