3c7b4228de18c81fdfd6c8ecfcb0d8a91aabf841
malok2012 <malok1433@hotmail.com>
2016-06-20 22:29:40 -0400

    Add all test to AllGameTest

--- test/AllGameTests.java
+++ test/AllGameTests.java
@@ -11,6 +11,23 @@ import org.junit.runners.Suite.SuiteClasses;
 import ability.TestPoison;
 import ability.TestPoke;
 import dungeon.TestDungeon;
+import item.TestAlittleDamage;
+import item.TestArmor;
+import item.TestGenericItem;
+import item.TestGun;
+import item.TestIgnoreDamage;
+import item.TestMega;
+import item.TestNormalArmor;
+import item.TestPokemon;
+import item.TestPotions;
+import item.TestPowerArmor;
+import item.TestReallyDamage;
+import item.TestSharpArmor;
+import item.TestSpear;
+import item.TestSpecialArmor;
+import item.TestSword;
+import item.TestUltra;
+import item.TestWeapon;
 import recovery.TestRecoveryLinear;
 import recovery.TestRecoveryNone;
 /**
@@ -21,7 +38,14 @@ import recovery.TestRecoveryNone;
 
 @RunWith(Suite.class)
 @SuiteClasses({TestRecoveryNone.class, TestRecoveryLinear.class, TestDungeon.class, TestCreature.class, TestLifeForm.class,
-	TestPlayer.class, TestMummy.class, TestGoblin.class, TestPoison.class, TestPoke.class})
+	TestPlayer.class, TestMummy.class, TestGoblin.class, TestPoison.class, TestPoke.class,
+	TestAlittleDamage.class, TestArmor.class, TestGenericItem.class,
+    TestGun.class, TestIgnoreDamage.class, TestMega.class, 
+    TestNormalArmor.class, TestPokemon.class, TestPotions.class, 
+    TestPowerArmor.class,TestReallyDamage.class, TestSharpArmor.class,
+    TestSpear.class, TestSpecialArmor.class, TestSword.class,
+    TestUltra.class, TestWeapon.class	
+})
 public class AllGameTests 
 {
 	
